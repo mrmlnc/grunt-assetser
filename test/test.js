fb03aa655c39f6ada33dc2574bc8ecd18e749f1e
Denis Malinochkin <dmalinochkin@rambler.ru>
2015-11-01 02:30:55 +0300

    update tests for ava 0.3.0

--- test/test.js
+++ test/test.js
@@ -2,71 +2,71 @@ var fs = require('fs');
 var test = require('ava');
 
 test('default space', function(t) {
-  var actual = fs.readFileSync('tmp/default.html');
-  var expected = fs.readFileSync('test/expected/default.html');
+  var actual = fs.readFileSync('../tmp/default.html', 'utf8');
+  var expected = fs.readFileSync('expected/default.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('default tab', function(t) {
-  var actual = fs.readFileSync('tmp/default_tab.html');
-  var expected = fs.readFileSync('test/expected/default_tab.html');
+  var actual = fs.readFileSync('../tmp/default_tab.html', 'utf8');
+  var expected = fs.readFileSync('expected/default_tab.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('marked default value', function(t) {
-  var actual = fs.readFileSync('tmp/marked_default.html');
-  var expected = fs.readFileSync('test/expected/marked_default.html');
+  var actual = fs.readFileSync('../tmp/marked_default.html', 'utf8');
+  var expected = fs.readFileSync('expected/marked_default.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('marked user value', function(t) {
-  var actual = fs.readFileSync('tmp/marked_mark.html');
-  var expected = fs.readFileSync('test/expected/marked_mark.html');
+  var actual = fs.readFileSync('../tmp/marked_mark.html', 'utf8');
+  var expected = fs.readFileSync('expected/marked_mark.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('only js', function(t) {
-  var actual = fs.readFileSync('tmp/type_js.html');
-  var expected = fs.readFileSync('test/expected/type_js.html');
+  var actual = fs.readFileSync('../tmp/type_js.html', 'utf8');
+  var expected = fs.readFileSync('expected/type_js.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('only css', function(t) {
-  var actual = fs.readFileSync('tmp/type_css.html');
-  var expected = fs.readFileSync('test/expected/type_css.html');
+  var actual = fs.readFileSync('../tmp/type_css.html', 'utf8');
+  var expected = fs.readFileSync('expected/type_css.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('only css and marked files', function(t) {
-  var actual = fs.readFileSync('tmp/type_css_and_marked_mark.html');
-  var expected = fs.readFileSync('test/expected/type_css_and_marked_mark.html');
+  var actual = fs.readFileSync('../tmp/type_css_and_marked_mark.html', 'utf8');
+  var expected = fs.readFileSync('expected/type_css_and_marked_mark.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('multiple assets', function(t) {
-  var actual = fs.readFileSync('tmp/multiple_assets.html');
-  var expected = fs.readFileSync('test/expected/multiple_assets.html');
+  var actual = fs.readFileSync('../tmp/multiple_assets.html', 'utf8');
+  var expected = fs.readFileSync('expected/multiple_assets.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('nesting space', function(t) {
-  var actual = fs.readFileSync('tmp/nesting_space.html');
-  var expected = fs.readFileSync('test/expected/nesting_space.html');
+  var actual = fs.readFileSync('../tmp/nesting_space.html', 'utf8');
+  var expected = fs.readFileSync('expected/nesting_space.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
 
 test('nesting tab', function(t) {
-  var actual = fs.readFileSync('tmp/nesting_tab.html');
-  var expected = fs.readFileSync('test/expected/nesting_tab.html');
+  var actual = fs.readFileSync('../tmp/nesting_tab.html', 'utf8');
+  var expected = fs.readFileSync('expected/nesting_tab.html', 'utf8');
   t.is(actual, expected);
   t.end();
 });
